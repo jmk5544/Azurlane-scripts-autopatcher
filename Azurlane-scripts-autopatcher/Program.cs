356cde99e17eb0e8472d8cca6757d5c46ee1ad3e
k0np4ku <nihilism.circle@gmail.com>
2018-06-17 03:30:12 +0700

    Add project files.

---
+++ Azurlane-scripts-autopatcher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Azurlane_scripts_autopatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
